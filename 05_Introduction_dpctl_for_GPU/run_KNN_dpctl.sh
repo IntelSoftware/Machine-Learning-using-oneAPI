6f73d6b5dc53c8bf2c05fcf3bbd3f9040f5ecdf2
bob.chesebrough <bob.chesebrough@intel.com>
2022-10-25 12:38:34 -0700

    renames, fix Teacherkit, README

--- 05_Introduction_dpctl_for_GPU/run_KNN_dpctl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
-/bin/echo "##" $(whoami) is compiling AI Essentials Module1 -- scikit-learn-Intelex_Intro - 4 of 5 KNN_dpctl.py
-python lab/compute_KNN_GPU.py
-
-
